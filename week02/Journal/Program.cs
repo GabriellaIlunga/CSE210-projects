c9e22e27d30e06bd6424a266e0593e9b8e101daa
GabriellaIlunga <gabriellamujingailunga@outlook.com>
2026-03-13 22:18:59 +0200

    Completed journal program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,74 @@
+//ADDED:Added a mood tracker# system that asks the user to rate their day on a scale of 1-10. This extra data is stored in the Entry class, saved to the file, and loaded back to help the user track emotional trends over time.
+
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal theJournal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+        string choice = "";
+
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        while (choice != "5")
+        {
+            Console.WriteLine("\nPlease select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine($"\n{prompt}");
+                Console.Write("> ");
+                string entryResponse = Console.ReadLine();
+
+                Console.Write("On a scale of 1-10, how was your mood today? ");
+                string mood = Console.ReadLine();
+
+                string dateText = DateTime.Now.ToShortDateString();
+
+                Entry newEntry = new Entry();
+                newEntry._date = dateText;
+                newEntry._promptText = prompt;
+                newEntry._entryText = entryResponse;
+                newEntry._mood = mood;
+
+                theJournal.AddEntry(newEntry);
+            }
+            else if (choice == "2")
+            {
+                theJournal.DisplayAll();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? ");
+                string fileName = Console.ReadLine();
+                theJournal.LoadFromFile(fileName);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("What is the filename? ");
+                string fileName = Console.ReadLine();
+                theJournal.SaveToFile(fileName);
+            }
+            else if (choice == "5")
+            {
+                Console.WriteLine("Goodbye.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid selection.");
+            }
+        }
     }
 }
